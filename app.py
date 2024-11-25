fcc565f41e1379d281234221cc6eed49bcb9efb2
Deepak Kumar Mohanty <99995298+Deepak8260@users.noreply.github.com>
2024-11-25 18:30:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import numpy as np
 pipe=pickle.load(open('pipe.pkl','rb'))
 df=pickle.load(open('df.pkl','rb'))
 
-st.title('Kumar Laptop Price Predictor')
+st.title('KD Laptop Price Predictor')
 
 # brand
 company = st.selectbox('Brand',df['Company'].unique())
@@ -46,7 +46,7 @@ hdd=st.selectbox('HDD(in GB)',[0,128,256,512,1024,2048])
 ssd=st.selectbox('SSD(in GB)',[0,8,128,256,512,1024])
 
 if st.button('predict price'):
-	if 'touchScreen'=='Yes':
+	if touchScreen=='Yes':
 		touchScreen=1
 	else:
 		touchScreen=0
@@ -58,11 +58,22 @@ if st.button('predict price'):
 
 	x_res= int(resolution.split('x')[0])
 	y_res = int(resolution.split('x')[1])
-	ppi = (((x_res**2) + (y_res**2))**0.5)/screen_size
+	try:
+		ppi = (((x_res ** 2) + (y_res ** 2)) ** 0.5) / screen_size
+	except ZeroDivisionError:
+		st.title("Screen Size cannot be zero. Please provide a valid value.")
+		ppi = 0 
+
+	#ppi = (((x_res**2) + (y_res**2))**0.5)/screen_size
 	querry = np.array([company,type,ram,gpu,weight,touchScreen,ips,ppi,cpu,hdd,ssd,os])
 
 	querry = querry.reshape(1,12)
-	st.title("The Predicted Price is: "+ str(int(np.exp(pipe.predict(querry))[0])))
+	if weight <= 0 or screen_size <= 0:
+		st.title('Please Enter all the details correctly')
+	elif hdd==0 and ssd==0:
+		st.title('Please select at least one storage option (HDD or SSD).')
+	else:
+		st.title("The Predicted Price is: "+ str(int(np.exp(pipe.predict(querry))[0])))
 
 
 
